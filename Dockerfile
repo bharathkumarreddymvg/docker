6478e57f32ce1d51b785db74206aa087114e3d25
mvg bharath kumar reddy <bharath0124r@gmail.com>
2025-06-10 11:38:57 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9
+
+WORKDIR /app
+
+COPY . .
+
+# Install numpy first to make sure it's available before others
+RUN pip install numpy==1.23.5
+
+# Now install everything else
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["python", "main.py"]
+
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin  # Import CORS
+import pandas as pd
+import numpy as np
+from flask import jsonify
+import pickle
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+pickle_in = open("hr_analytics.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return "hello welcome"
+
+
+
+@app.route('/predict')
+def prediction_authentication():
+    try:
+        experience = float(request.args.get("experience"))
+        test_score = float(request.args.get("test_score"))
+        interview_score = float(request.args.get("interview_score"))
+        prediction = classifier.predict([[experience, test_score, interview_score]])
+        return jsonify({"prediction": prediction[0]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run()
